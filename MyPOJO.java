4f3888d62802ff9bd1675c0666b5f22dba3bd0a7
indrapriyanka <indrapriyanka88@gmail.com>
2017-03-25 11:19:51 -0700

    Google Maps
    Google Maps for Android.

---
+++ MyPOJO.java
@@ -0,0 +1,26 @@
+package com.example.googleplacestest;
+
+public class MyPOJO 
+{
+
+	String name,vicinity;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getVicinity() {
+		return vicinity;
+	}
+
+	public void setVicinity(String vicinity) {
+		this.vicinity = vicinity;
+	}
+	
+	
+	
+}
